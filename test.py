25630f5f804fdfacc9abe77d28112a8d0aa5cb40
Henogapok <kskfbdoc@gmail.com>
2024-10-02 22:27:23 +0500

    [add] что-то сделал, что-то получилось

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import math
 import random
-
+from sklearn.datasets import load_iris
 import numpy as np
 
-w = [0, 0, 0]
+w = [0, 0, 0, 0]
 
 
 # Функция активации
@@ -31,13 +31,24 @@ def train(data, exp, LR=0.1, EPOCH=50):
 
 
 def main():
-    trainingdata = [[0, 0, 1], [0, 1, 1], [1, 0, 1], [0, 1, 0]]
-    expresults = [0, 0, 1, 0]
+    iris = load_iris()
+    trainingdata = iris['data']
+    expresults = iris['target']
+
+    binary_target = np.where(expresults == 0, 0, 1)
+
     for i in range(len(w)):
         w[i] = random.uniform(0, 1.05)
-    train(trainingdata, expresults)
-    new = [1, 0, 0]
-    print(activate(rightProp(new)))
+
+    train(trainingdata, binary_target)
+    new = [5.1, 3.5, 1.4, 0.2]
+    prediction = activate(rightProp(new))
+    print(prediction)
+
+    if prediction < 0.5:
+        print("Это Setosa")
+    else:
+        print("Это не Setosa")
 
 
 if __name__ == "__main__":
